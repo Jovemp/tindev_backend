940ad9931c1db6ae94be0049fe51d0f359eae4c1
Paulo <jovemp@hotmail.com>
2019-08-09 17:46:08 -0300

    Adicionando socket

--- src/server.js
+++ src/server.js
@@ -3,15 +3,21 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const routes = require('./routes.js');
 
-const server = express();
+const app = express();
+const server = require("http").Server(app);
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+    console.log('Nova conexao', socket.id);
+})
 
 mongoose.connect('mongodb+srv://psousa:P@ssw0rd@cluster0-8qtex.mongodb.net/omnistack?retryWrites=true&w=majority',
-    { 
+    {
         useNewUrlParser: true
     });
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(3333);
